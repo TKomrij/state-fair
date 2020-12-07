93eba1336d145baab0d56f465bce26beff688228
TKomrij <tatiane.b.komrij@gmail.com>
2020-12-07 09:28:38 -0600

    made tickets each time button was pressed

---
+++ scripts/rides/RideTicketHolders.js
@@ -0,0 +1,17 @@
+// const contentTarget = document.querySelector(".rides")
+// const eventHub = document.querySelector("#state-fair")
+
+// export const RideTicketHolders = () => {
+//     eventHub.addEventListener("click", customEvent => {
+//         const userChoiceElement = document.querySelector(".food")
+//         userChoiceElement.innerHTML = customEvent.detail.selectedFood
+//     })
+// }
+
+// export const TicketBooth = () => {
+//     contentTarget.innerHTML = `
+//         <div class="foodBooth">
+//             <button id="foodTicket">Food Ticket</button>
+//         </div>
+//     `
+// }
