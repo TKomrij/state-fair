e065bc7f5ae9ebc3512005ce1e5c4f28ecdc046b
TKomrij <tatiane.b.komrij@gmail.com>
2020-12-07 13:00:22 -0600

    work on event listener for button

--- scripts/TicketBooth.js
+++ scripts/TicketBooth.js
@@ -1,10 +1,35 @@
 const contentTarget = document.querySelector(".entry")
 const eventHub = document.querySelector("#state-fair")
 
-eventHub.addEventListener("click", customEvent => {
-    const userChoiceElement = document.querySelector(".rides")
-    userChoiceElement.innerHTML += customEvent.detail.selectedTicket
+const ticketSelectedEvent = new CustomEvent("ticketSelected", {
+    detail: {
+        selectedTicket: "Ride"
+    }
 })
+eventHub.dispatchEvent(ticketSelectedEvent)
+
+
+eventHub.addEventListener("ticketSelected", customEvent => {
+    contentTarget.innerHTML = customEvent.detail.selectedTicket
+})
+
+
+
+
+
+
+
+// eventHub.addEventListener("click", event => {
+//     if(event.target.id === "rideTicket")
+//     const rideEvent = new CustomEvent("rideTicketPurchased", {
+//         detail: {
+//             ticket: event.target.value
+//         }
+//     })
+ 
+//     eventHub.dispatchEvent(rideEvent)
+// })
+
 
 export const TicketBooth = () => {
     contentTarget.innerHTML = `
@@ -15,3 +40,5 @@ export const TicketBooth = () => {
     `
 }
 
+// const userChoiceElement = document.querySelector(".rides")
+// userChoiceElement.innerHTML = event.detail.ticket
